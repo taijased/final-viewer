0019cf483e8c7c971cbf116d2266c119774c3480
Spiridonov Maxim <taijased@gmail.com>
2020-02-18 14:41:46 +0300

    Initial Commit

---
+++ swift-starter-kit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-starter-kit
+//
+//  Created by Максим Спиридонов on 18.02.2020.
+//  Copyright © 2020 Максим Спиридонов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
