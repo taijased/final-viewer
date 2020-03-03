fbbe3497e71e07d2ed4076ea73d136a78f8279bc
Spiridonov Maxim <taijased@gmail.com>
2020-03-03 15:29:56 +0300

    add dark mode

---
+++ viewer-final/Design System/UIAlert/UIAlertController + custom.swift
@@ -0,0 +1,9 @@
+//
+//  UIAlertController + custom.swift
+//  viewer-final
+//
+//  Created by Максим Спиридонов on 03.03.2020.
+//  Copyright © 2020 Максим Спиридонов. All rights reserved.
+//
+
+import Foundation
